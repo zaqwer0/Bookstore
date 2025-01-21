58e028b9817d8f918fd6991f523dabf4902a33cd
Yegor Ankudovich <yahor.ankudovich@innowise.com>
2025-01-21 10:33:47 +0100

    initial commit

---
+++ src/test/java/by/example/bookstore_api/BookStoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package by.example.bookstore_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookStoreApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
