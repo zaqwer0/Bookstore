a0304f47c453f99528d388fbde28eca2a758649c
Yegor Ankudovich <yahor.ankudovich@innowise.com>
2025-03-06 09:49:43 +0100

    INNOW-3: cash throw redis by serialization (next step will be probly JSON as a value)

--- src/main/java/by/example/bookstore/api/model/dto/response/BookResponseDto.java
+++ src/main/java/by/example/bookstore/api/model/dto/response/BookResponseDto.java
@@ -2,13 +2,14 @@ package by.example.bookstore.api.model.dto.response;
 
 import lombok.Builder;
 
+import java.io.Serializable;
 import java.util.UUID;
 
 @Builder
-public record BookResponseDto(
+public record BookResponseDto (
     UUID id,
     String title,
     String authorName,
     String authorLastName,
     double price,
-    Integer publishedYear) {}
+    Integer publishedYear) implements Serializable {}
